2478594c8130e4ea8a730fa6b9f97fa47be5d0a6
Beyhan Tüzün <58696723+beyhantuzun@users.noreply.github.com>
2020-12-22 21:29:50 +0300

    Add files via upload

---
+++ python_homework.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#value= input("please enter a value:" )
+#typ=type(value)
+#print(f"type of value you entered: {typ}" )
+
+
+#data= input ("number:" )
+#number = int(data)
+#typ=type(data)
+#print(f"square of the entered number: {number ** 2}")
+#print(f"type of number  you entered: {typ}")
+
+#data2 = int(input("number1:" ))
+#data3 = int(input("number2:" ))
+#print(data2, "+", data3, "=", data2+data3)
+#print(type(data2+data3))
+
+
+#v= input("what's your name:" )
+#print("my name is: {}" .format(v))
+#print (type(v))
+
+#s1 = int(input("s1:" ))
+#s2= float(input("s2:" ))
+#print(s1, "/", s2, "=", s1/s2)
+#print (type(s1/s2))
+
+
+
+
+
+
+
+
+
+
